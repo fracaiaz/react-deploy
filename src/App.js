7dd1ef41febcedf1af58c080cbf7fd6c76de716a
fracaiaz <francescorosariocaiazzo@gmail.com>
2021-09-03 15:36:59 +0200

    caccia

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import profileImage from './img/hp.png'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>CACCIA AL TESORO 2021</h1>
+      <h2>Author: fracaiaz</h2>
+      <img src={profileImage} alt="profile-image"/>
     </div>
   );
 }
