bf5cdf3034cecd7b5d36a038f545e6f0e4a55761
fracaiaz <francescorosariocaiazzo@gmail.com>
2021-09-04 17:41:42 +0200

    sdfmsdmfisdf

--- src/App.js
+++ src/App.js
@@ -2,6 +2,25 @@ import logo from './logo.svg';
 import './App.css';
 import profileImage from './img/hp.png'
 import profileImage2 from './img/harry.png'
+const isMobileDevice = useMediaQuery({
+  query: "(min-device-width: 480px)",
+});
+
+const isTabletDevice = useMediaQuery({
+  query: "(min-device-width: 768px)",
+});
+
+const isLaptop = useMediaQuery({
+  query: "(min-device-width: 1024px)",
+});
+
+const isDesktop = useMediaQuery({
+  query: "(min-device-width: 1200px)",
+});
+
+const isBigScreen = useMediaQuery({
+  query: "(min-device-width: 1201px )",
+});
 
 function App() {
   console.log("hai risolto il misfatto")
@@ -9,6 +28,13 @@ function App() {
     <div className="App">
       <h1>CACCIA AL TESORO 2021</h1>
       <h2>Azione cattolica ACG Santa Maria la Carità </h2>
+      {isMobileDevice && <Mobile />}
+  {isTabletDevice && <>
+  <TabletMobile />
+  {isDesktop && <Desktop />}
+  {isLaptop && <Laptop />}
+  {isBigScreen && <BigScreen />}
+</>}
       <img src={profileImage} alt=""/>
       <img src={profileImage2} alt="profile-image"/>
 
