50dece300916cce8d0b39be5a007ada17c3507a2
fracaiaz <francescorosariocaiazzo@gmail.com>
2021-09-04 23:36:46 +0200

    ehi

--- src/App.js
+++ src/App.js
@@ -11,22 +11,18 @@ function App() {
   console.log("hai risolto il misfatto")
   return (
     <div className="App">
-      <Container>
-        <Row>
+      
+         
       <h1>CACCIA AL TESORO 2021</h1>
       <h2>Azione cattolica ACG Santa Maria la Carità </h2>
-      </Row>
-      <Row>
-        <Col sm="6">
+    
+    
       <img src={profileImage} alt=""/>
-      </Col>
-      <Col sm="6"></Col>
-      </Row>
-      <Row>
-      
+     
+     
+
       <img src={profileImage2} alt="profile-image"/>
-      </Row>
-      </Container>
+
     </div>
     
   );
