f947de1d752b57882f21b95d1cf2457b15e1e556
fracaiaz <francescorosariocaiazzo@gmail.com>
2021-09-06 16:16:18 +0200

    ehi

--- src/App.js
+++ src/App.js
@@ -23,9 +23,10 @@ function useWindowSize(){
 
 function App() {
 
-sessionStorage.setItem("Pontone")
-  
+
+  console.log("ecco un alto consiglio -> DECODE BASE 64")
   console.log("TGEgc29sdXppb25lIGFsbCdlbmlnbWEgbGEgdHJvdmkgbmVsIGxvY2FsIHN0b3JhZ2UuClNlIHNlaSBhcnJpdmF0byBmaW5vIGEgcXVpLCBhdnJhaSBzaWN1cmFtZW50ZSB2aXN0byBjaGUgcXVhbmRvIGhhaSBjbGljY2F0byBzdSBpc3BlemlvbmEgc2kgw6ggYXBlcnRhIHVuYSB0YWJlbGxhLgpRdWVzdGEgaGEgdW5hIHNlcmllIGRpIFRBQi4gU2UgY2xpY2NoaSBzdSBBcHBsaWNhdGlvbiwgdGkgc2kgYXByaXLDoCB1biBtZW7DuSBhIHRlbmRpbmEgc3VsbGEgc2luaXN0cmEsIGNsaWNjYSBzdSBMb2NhbCBTdG9yYWdlIGUgZ3VhcmRhIGFsIGNlbnRybyBkZWxsYSBwYWdpbmEsIG5lbCBjYW1wbyBWYWx1ZS4=")
+  sessionStorage.setItem("luogo","Pontone")
   return (
     <div className="App">
 
@@ -45,6 +46,7 @@ sessionStorage.setItem("Pontone")
       <img src={profileImage2} alt="profile-image"/>
 
     </div>
+    
     
   );
 }
