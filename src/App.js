0e585434def16ece2bfc3d7e2c1ed390a0a1460c
fracaiaz <francescorosariocaiazzo@gmail.com>
2021-09-06 16:05:40 +0200

    ahahahah

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import profileImage2 from './img/harry.gif'
 import { Container,Row,Col } from 'reactstrap'
 import React, {useState, useEffect} from 'react';
 import {BrowserView, MobileView} from 'react-device-detect';
+import ss from "session-storage"
 
 function useWindowSize(){
   const [size,setSize] = useState([window.innerHeight,window.innerWidth]);
@@ -22,9 +23,9 @@ function useWindowSize(){
 
 function App() {
 
-
+sessionStorage.setItem("Pontone")
   
-  console.log("hai risolto il misfatto")
+  console.log("TGEgc29sdXppb25lIGFsbCdlbmlnbWEgbGEgdHJvdmkgbmVsIGxvY2FsIHN0b3JhZ2UuClNlIHNlaSBhcnJpdmF0byBmaW5vIGEgcXVpLCBhdnJhaSBzaWN1cmFtZW50ZSB2aXN0byBjaGUgcXVhbmRvIGhhaSBjbGljY2F0byBzdSBpc3BlemlvbmEgc2kgw6ggYXBlcnRhIHVuYSB0YWJlbGxhLgpRdWVzdGEgaGEgdW5hIHNlcmllIGRpIFRBQi4gU2UgY2xpY2NoaSBzdSBBcHBsaWNhdGlvbiwgdGkgc2kgYXByaXLDoCB1biBtZW7DuSBhIHRlbmRpbmEgc3VsbGEgc2luaXN0cmEsIGNsaWNjYSBzdSBMb2NhbCBTdG9yYWdlIGUgZ3VhcmRhIGFsIGNlbnRybyBkZWxsYSBwYWdpbmEsIG5lbCBjYW1wbyBWYWx1ZS4=")
   return (
     <div className="App">
 
