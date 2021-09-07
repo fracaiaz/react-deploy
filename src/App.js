c9524c75d99d54425e180937fc7f32a9c18966f2
fracaiaz <francescorosariocaiazzo@gmail.com>
2021-09-07 14:34:26 +0200

    ehi

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ import profileImage3 from './img/bacchetta.png'
 import { Container,Row,Col } from 'reactstrap'
 import React, {useState, useEffect} from 'react';
 import {BrowserView, MobileView} from 'react-device-detect';
-
+import { icons } from 'react-icons';
+import { FaTrashAlt, FaInfoCircle, FaEye, FaEyeSlash, IoMdLaptop, IoIosColorWand, IoIosDesktop} from 'react-icons/fa';
 function useWindowSize(){
   const [size,setSize] = useState([window.innerHeight,window.innerWidth]);
   useEffect(()=>{
@@ -21,26 +22,40 @@ function useWindowSize(){
   return size;
 }
 
+
 function App() {
+  const [isImg, setImg] = useState(false)
+  const appari = () => {
+    setImg(true)
+  };
 
+  const appari2 = () => {
+    setImg(false)
+  };
 
-  console.log(" -> DECODE BASE 64")
+  console.log(" prova ad usare-> DECODE BASE 64")
   console.log("aHR0cHM6Ly93d3cuYmFyZ2lvcm5hbGUuaXQvd3AtY29udGVudC91cGxvYWRzL3NpdGVzLzQvMjAxNi8wMy95b2dhLmpwZw==")
  
   return (
     <div className="App">
 
-         
+        
       <h1>CACCIA AL TESORO 2021</h1>
       <h2>Azione cattolica ACG Santa Maria la Carità </h2>
       <>
   <BrowserView>
-  <img src={profileImage} alt=""/>
-  
+  <img src={profileImage} alt=""/>  
   </BrowserView>
   <MobileView>
-      <img src={profileImage3} alt=""/>
-      <h3>tip= Open it on your PC</h3>
+      
+  { isImg && <button className="button" onClick={ appari2}> Open it on your PC
+  <i className="fa fa-heart-o" />
+          </button>}
+  { !isImg &&
+ <FaEye className="button" onClick={ appari}> Clicca
+           <icons className="pe-7s-shopbag" />
+          </FaEye>
+}
   </MobileView>
 </>
       
